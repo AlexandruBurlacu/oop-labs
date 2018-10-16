0d01ef83b623054073d0c3629a89ab0e4f1745de
Alex Burlacu <alexburlacu96@gmail.com>
2018-10-16 21:55:04 +0300

    update .gitignore, add visitor example and some structure to the bridge example

--- ruby-design-patterns/lab2/bridge_example.rb
+++ ruby-design-patterns/lab2/bridge_example.rb
@@ -4,4 +4,43 @@
 """
 
 
+class ComputingDevice
+  # This is the Abstract Abstraction :D and a Facade too
+  def initialize(implementation)
+    @implementation = implementation
+  end
 
+  def run
+    raise NotImplementedError.new
+  end
+end
+
+# Concrete Abstractions / Facades
+
+class Mobile < ComputingDevice
+end
+
+class HomePCDevice < ComputingDevice
+end
+
+class AFuckinWorkstation < ComputingDevice
+end
+
+# Concrete Implementors
+
+class Components
+end
+
+class EnergyEfficientComponents < Components
+end
+
+class LowBudgetComponents < Components
+end
+
+class HighEndComponents < Components
+end
+
+END {
+  computing_device = Mobile.new(EnergyEfficientComponents.new)
+  computing_device.run
+}
--- ruby-design-patterns/lab2/visitor_example.rb
+++ ruby-design-patterns/lab2/visitor_example.rb
@@ -16,4 +16,40 @@ class Visitor
 end
 
 # TODO implement Concrete products and Visitors
+class Computer
+  include Visitable
 
+  attr_reader :cpu, :gpu, :storage, :ram
+
+  def initialize
+    @cpu = "Intel CPU"
+    @gpu = "Nvidia GPU"
+    @storage = "Samsung SSD"
+    @ram = "32 DDR4"
+  end
+end
+
+class PlaySomeGames < Visitor
+  def visit(computer)
+    "Playing GTA V on a computer with #{computer.cpu}, #{computer.gpu} and damned #{computer.ram}"
+  end
+end
+
+class EditSomePhotos < Visitor
+  def visit(computer)
+    "Editig photos in Photoshop CC 6 on a computer with #{computer.cpu}, an #{computer.storage} and just #{computer.ram}"
+  end
+end
+
+class ExplodeComputer < Visitor
+  def visit(computer)
+    "Fuckin ruined a computer with #{computer.cpu}, #{computer.gpu}, #{computer.storage} and #{computer.ram}. Shame on you!"
+  end
+end
+
+END {
+  computer = Computer.new
+  p computer.accept EditSomePhotos.new
+  p computer.accept PlaySomeGames.new
+  p computer.accept ExplodeComputer.new
+}
